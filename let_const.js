343dc34d5359b1e12a0e3e359294ca1c5555b81b
Shirnae Johnson <shirnaejohnson@Shirnaes-MacBook-Air.local>
2021-02-15 16:04:45 -0600

    let and const ex

---
+++ let_const.js
@@ -0,0 +1,19 @@
+const PI = 3.14;
+
+
+
+
+// QUIZ
+
+//What is the difference between var and let?
+// var can be reassigned and redeclared whereas let can only be reassigned
+// var had function scope and let has block scope
+//What is the difference between var and const?
+// var can be reassigned and redeclared whereas const can not be reassigned
+//or redeclared. var has function scope and const has block scope
+//What is the difference between let and const?
+//let can only be reassigned whereas const can not be reassigned or redeclared, both 
+// have block scope
+//What is hoisting?
+// Hoisting is when the delcaration 
+//is ran seperate from the intialization.
